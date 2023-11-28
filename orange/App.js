399ae8f308b550e5c15367e14e0bc51929c42918
jaswanthinvally <jaswanthvenkat001@gmail.com>
2023-11-28 17:24:24 +0530

    login and signin pages crearted

--- orange/App.js
+++ orange/App.js
@@ -1,20 +1,20 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, Button } from 'react-native';
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+    <Text style={styles.login}>login</Text>
+
     </View>
   );
 }
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
+  login:{
+
+
+  }
+
 });
+
